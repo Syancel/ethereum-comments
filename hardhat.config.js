7c82be9a43954d909b201fa8984789bd96ca2527
Marcel Schmitt <schmitt.marcel97@gmail.com>
2021-06-08 10:29:01 +0200

    Configure Hardhat for Kovan testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require("dotenv");
+require("@nomiclabs/hardhat-ethers");
+
+const { ALCHEMY_KOVAN_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "kovan",
+  networks: {
+    hardhat: {},
+    kovan: {
+      url: ALCHEMY_KOVAN_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
